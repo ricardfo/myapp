d9b79880136ed91291cea6dc7a80f0c6ae1f5b8c
Ricardo Fontoura <ricardfo@usp.br>
2020-06-15 17:47:49 -0300

    add new routes, controller e view.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,71 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/login', function () {
+    return 'login';
+})->name('login');
 
+/*
+Route::middleware([])->group( function () {
+    Route::prefix('/admin')->group( function() {
+        Route::name('admin.')->group( function () {
+        Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+
+        Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+        Route::get('/produtos', 'TesteController@teste')->name('produtos');
+
+        Route::get('/', 'TesteController@teste')->name('home');
+        });
+    });
+});*/
+
+# ou
+
+Route::group([
+    'middleware' => ['auth'],
+    'prefix' => 'admin',
+    'name' => 'admin',
+], function () {
+        Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+
+        Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+        Route::get('/produtos', 'TesteController@teste')->name('produtos');
+
+        Route::get('/', 'TesteController@teste')->name('home');
+});
+
+
+Route::redirect('redirect1', 'redirect2');
+#Route::get('redirect1', function () {
+#    return redirect('/redirect2');
+#});
+Route::get('redirect2', function () {
+    return 'redirect2';
+});
+Route::get('/produtos/{idProduto?}', function ($idProduto = '') {
+    return "Produto(s) {$idProduto}";
+});
+Route::get('/categorias/{flag}/produtos', function ($flag) {
+    return "Produto da categoria: $flag";
+});
+Route::get('/categoria/{cat}', function ($cat) {
+    return "Produto da categoria: $cat";
+});
+Route::match(['get','post'],'/match', function () {
+    return 'Match';
+});
+Route::any('/any', function () {
+    return 'Any';
+});
+Route::post('/register', function () {
+    return ;
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá';
 });
